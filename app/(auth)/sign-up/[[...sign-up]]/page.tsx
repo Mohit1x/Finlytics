60cf26409143b7048f25fc5cdf40c15ccdfde493
mohit1x <mohitvyas746@gmail.com>
2025-08-22 00:02:41 +0530

    added clerk authentication

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,28 @@
-const SignUpPage = () => {
+import { ClerkLoaded, ClerkLoading, SignUp } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
+
+export default function Page() {
   return (
-    <div>
-      <h1>Sign up page</h1>
+    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+      <div className="h-full lg:flex flex-col items-center justify-center px-4">
+        <div className="text-center space-y-4 pt-16">
+          <h1 className="font-bold text-3xl text-[#2E2A47]">Welcome</h1>
+          <p className="text-base text-[#7E8CA0]">
+            Welcome please create your account to access features!
+          </p>
+        </div>
+        <div className="mt-8 flex items-center justify-center">
+          <ClerkLoaded>
+            <SignUp path="/sign-up" />
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2 className="animate-spin text-muted-foreground" />
+          </ClerkLoading>
+        </div>
+      </div>
+      <div className="hidden lg:flex items-center justify-center bg-blue-600">
+        <h1 className="text-white text-3xl font-semibold">Finlytics</h1>
+      </div>
     </div>
   );
-};
-
-export default SignUpPage;
+}
