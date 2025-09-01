80146d4b2a3c3f95d41720b6a8c2fe0164be1001
Mohit1x <mohitvyas746@gmail.com>
2025-09-01 21:49:30 +0530

    added new account sidebar and also installed zustand

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,16 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
+  const { onOpen } = useNewAccount();
+
   return (
     <div>
       <UserButton />
+      <Button onClick={onOpen}>Add a account</Button>
     </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,9 @@ import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 
 import "./globals.css";
+
 import { QueryProvider } from "@/providers/query-provider";
+import { SheetProvider } from "@/providers/sheet-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +23,10 @@ export default function RootLayout({
     <ClerkProvider afterSignOutUrl={"/"}>
       <html lang="en">
         <body className={inter.className}>
-          <QueryProvider>{children}</QueryProvider>
+          <QueryProvider>
+            <SheetProvider/>
+            {children}
+            </QueryProvider>
         </body>
       </html>
     </ClerkProvider>
